f81a40f2da261c62bc0f03ab326495a74d8e305f
KLL <1247703485@qq.com>
2021-05-06 17:31:12 +0800

    [update] 20210505

--- hooks/git-pre-add.js
+++ hooks/git-pre-add.js
@@ -6,21 +6,24 @@ const path = require('path');
 const [execPath, binPath, ...arg] = process.argv;
 
 function main() {
-  const gitMdCheckStatus = execSync('git status --porcelain')
-                    .toString()
-                    .trim()
-                    .split('\n')
-                    .map(item => item.split(/\s+/).filter(str => str.replace(/\s+/))[1])
-                    .map(item => path.join(__dirname, '../', item))
-                    .filter(item => fs.statSync(item).isFile())
-                    .filter(item => item.substr(item.lastIndexOf('.')) === '.md');
-  // 读取文件内容
+  const gitFileStatus = execSync('git status --porcelain')
+    .toString()
+    .trim()
+    .split('\n')
+    .map(item => item.split(/\s+/).filter(str => str.replace(/\s+/))[1])
+    .map(item => path.join(__dirname, '../', item))
+    .filter(item => fs.statSync(item).isFile());
+  // 对图片进行压缩
+  // const gitImgCheckStatus = gitFileStatus.filter(item => ['.jpg', '.jpeg', '.png'].includes(item.substr(item.lastIndexOf('.'))));
+  // execSync(`git add ${gitImgCheckStatus.join(' ')}`);
+  // 处理markdown图片引入
+  const gitMdCheckStatus = gitFileStatus.filter(item => item.substr(item.lastIndexOf('.')) === '.md');
   gitMdCheckStatus.map(filePath => {
     const file = fs.readFileSync(filePath, 'utf8');
     const resultData = file.replace(/(?<=\!\[.*?\])\((?!http|https)(.*?)\)/g, '(https://raw.githubusercontent.com/shamerKill/shamerKill.github.io/master/$1)');
     fs.writeFileSync(filePath, resultData, 'utf8');
   })
-  execSync(`git add ${gitMdCheckStatus.join(' ')}`);
+  // execSync(`git add ${gitMdCheckStatus.join(' ')}`);
 }
 
 try {
